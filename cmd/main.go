8424be3b65148d309c9fc1c7731333931b892648
Daniil Trishkin <kernel.pryanic@protonmail.com>
2024-10-01 21:24:04 +0200

    cosmetic update

--- cmd/main.go
+++ cmd/main.go
@@ -9,18 +9,10 @@ import (
 
 func main() {
 	seed := time.Now().UnixNano()
-	fmt.Printf("Seed: %d\n", seed)
+	fmt.Printf("seed: %d\n", seed)
 	headers := useragent.NewUserAgent(15, seed).Headers
 
-	fmt.Println("Generated Headers:")
 	for key, value := range headers {
 		fmt.Printf("%s: %s\n", key, value)
 	}
-
-	// start := time.Now()
-	// for i := 0; i < 1000; i++ {
-	// 	seed := time.Now().UnixNano()
-	// 	useragent.NewUserAgent(15, seed)
-	// }
-	// fmt.Printf("Generated 1000 user agents in %s\n", time.Since(start))
 }
