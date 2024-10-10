f0df5a78c9fa29f89c054f376140f018696c3fcb
Daniil Trishkin <kernel.pryanic@protonmail.com>
2024-10-10 22:58:10 +0200

    init shit

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/chinese-room-solutions/fakebro/useragent"
+	"github.com/chinese-room-solutions/fakebro/webgl"
 )
 
 func main() {
@@ -15,4 +16,7 @@ func main() {
 	for key, value := range headers {
 		fmt.Printf("%s: %s\n", key, value)
 	}
+
+	renderer := webgl.NewWebGLRenderer("Windows", "10.0.0", 12345)
+	fmt.Println(renderer.Renderer)
 }
